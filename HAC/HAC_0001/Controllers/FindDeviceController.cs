a9c6976629efd1a29f24a4ecb88b8336997e6b4c
akasoggybunz <somedudenamedaustin@gmail.com>
2018-12-19 17:01:45 -0700

    Adding sniff logic to find device, returning some generic data. Not sure if correct ip address.

--- HAC/HAC_0001/Controllers/FindDeviceController.cs
+++ HAC/HAC_0001/Controllers/FindDeviceController.cs
@@ -2,24 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HAC.Models;
 using Microsoft.AspNetCore.Mvc;
+using SharpPcap.AirPcap;
+using SharpPcap.WinPcap;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace HAC.Controllers
 {
     [Route("api/[controller]")]
-    public class FindDeviceController : Controller
+    public class FindDeviceController : ControllerBase
     {
+        /// <summary>
+        /// Sniff devices on the network.
+        /// </summary>
+        /// <returns>List of Sniff Devices</returns>
         // GET: api/<controller>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<SniffDevice> Get()
         {
-
+            List<SniffDevice> devices = new List<SniffDevice>();
             DeviceFinder.Sniffer networkSniffer = new DeviceFinder.Sniffer();
             var traffic = networkSniffer.sniff();
-            
-            return new string[] { traffic[1].Name, traffic[2].Name };
+
+            // Build output using Model
+            foreach (WinPcapDevice t in traffic)
+            {
+                devices.Add(new SniffDevice()
+                {
+                    Name = t.Name,
+                    Description = t.Description,
+                    IpAddress = t.Addresses.Select(x=>x.Addr.ipAddress).ToString(),
+                });
+            }
+            return devices;
         }
 
         // GET api/<controller>/5
